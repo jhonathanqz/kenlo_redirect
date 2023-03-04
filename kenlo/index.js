c20a8ca3c378a6cfad2bd92cd0ed29df213fc777
jhonathanqz <jhonathanqz011@gmail.com>
2023-03-04 13:30:30 -0300

    Configure new host and function get list

--- kenlo/index.js
+++ kenlo/index.js
@@ -1,40 +1,63 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-      redirectKenlo(res);
-      res.end();
-    } else if (req.url.includes('kenlo.com.br')) {
-        if(req.url.includes('.br/')){
-            const urlEdit = req.url.split('br/');
-            if(urlEdit.length > 1){
-             //   redirectKenlo(res, urlEdit[1]);
-                return
-            }
-        }
-        redirectKenlo(res);
-        
-      res.end();
-    } else {
-      res.write('Página não encontrada.');
-      res.end();
+app.use((req, res, next) => {
+  if (req.headers.host === 'localhost:3000' || req.headers.host === 'localhost' ) {
+    /*
+    Para fins de testar se o código funciona foi feita a verificação com "localhost:3000",
+    porém se fosse em produção, a verificação acima seria req.headers.host === 'kenlo.com.br'
+    */
+    if (req.url.includes('kenlo.com.br')) {
+      if (req.url.includes('.br/')) {
+        const urlEdit = req.url.split('br/');
+        if(urlEdit.length > 1){
+          return redirectKenlo(res, urlEdit[1]);
+         }
+      }
+      return redirectKenlo(res);
     }
-  });
-
-  function redirectKenlo(res, params) {
-    if(params != null){
-        res.writeHead(302, {
-            'Location': 'https://www.kenlo.com.br/' + params
-          });
-        return;
-    }
-    res.writeHead(302, {
-        'Location': 'https://www.kenlo.com.br'
-      });
+    
+    return pageNotFound(res);
   }
-  
-  server.listen(3000, () => {
-    console.log('Servidor iniciado na porta 3000');
-  });
+  next();
+});
 
-  
+
+function redirectKenlo(res, params) {
+  if (params != null) {
+    if (params.includes('lista')) {
+      return buildReturn(res);
+    }
+    return res.redirect(`https://www.kenlo.com.br/${params}`);
+  }
+  res.redirect(`https://www.kenlo.com.br`);
+}
+
+function pageNotFound(res) {
+  res.send('Desculpe, não encontramos nenhuma página com este endereço');
+}
+
+function buildReturn(res) {
+  const listReturn = {
+    "total": 10,
+    "items": [
+      {
+        "id": 1,
+        "cidade": "Campinas",
+        "latitude": "-22.8951606",
+        "longitude": "-47.1711205, 11",
+    },
+      {
+        "id": 2,
+        "cidade": "Valinhos",
+        "latitude": "-22.9860169",
+        "longitude": "-47.0888407"
+      }
+    ]
+  };
+  res.send(listReturn);
+}
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000!');
+});
